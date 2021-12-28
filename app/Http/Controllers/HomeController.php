e636080b0beefda38767bed7d5141c6fe2be549b
usmanIN <61617956+usmanIN@users.noreply.github.com>
2021-12-28 18:49:35 +0530

    updated add to card

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,17 +33,21 @@ class HomeController extends Controller
     
     public function display($slug){
         $result['products'] = DB::table('products')->where(['product_slug'=>$slug])->get();        
+
         $tempTable = DB::table('products_images')->where(['product_id'=>$result['products'][0]->id])->get();        
+
         if($tempTable->count() > 0){
             $result['products_extra_images'] = $tempTable;
         }
-        $tempTable = DB::table('products_attribute')->where(['product_id'=>$result['products'][0]->id])->get();                
+
+        $tempTable = DB::table('products_attribute')
+        ->leftJoin('colors','colors.id','products_attribute.color')
+        ->where(['products_attribute.product_id'=>$result['products'][0]->id])->get();                
+
         if($tempTable->count() > 0){
             $result['products_attributes'] = $tempTable;
-            foreach($tempTable as $key => $list){
-                $result['colors'][$key] = DB::table('colors')->where(['id'=>$list->color])->get();
-            }
         }
+
         $tempTable = DB::table('products')->where(['category_id'=>$result['products'][0]->category_id,'product_status'=>1])->get();
         if($tempTable->count() > 0){
             $result['related_products'] = $tempTable;
@@ -53,7 +57,7 @@ class HomeController extends Controller
 
         
         // echo "<pre>";
-        // print_r($result['colors']);
+        // print_r($result);
         // die();
         return view('pages.display',$result); 
         
@@ -67,6 +71,19 @@ class HomeController extends Controller
     public function store(Request $request)
     {
         //
+        $success = false;
+        $message = "Failed! Add To Cart";
+        $product_id = $request->post('product_id');
+        $product_quantity = $request->post('product_quantity');
+        $product_attributes_id = $request->post('product_attributes_id');
+        
+        if($request->session()->has("customerLogin")){
+            $getID = $request->session()->get('customerId');
+        }else{
+            $getID = $this->getRandomId();
+        }
+
+        //return response()->json(['success' =>$success,'message'=>$message]);
     }
 
     public function show(Home $home)
@@ -87,5 +104,15 @@ class HomeController extends Controller
     public function destroy(Home $home)
     {
         //
+    }
+
+    private function getRandomId(){
+        if(session()->has('tempRandomId')){
+            $random =  session()->get('tempRandomId');            
+        }else{
+            $random = time();
+            session()->put('tempRandomId',$random);
+        }
+        return $random;
     }
 }
